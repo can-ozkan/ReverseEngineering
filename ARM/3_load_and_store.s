fa3b7591c85c59bbf5b1d12794739dfae4441259
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2025-04-25 14:26:21 +0200

    Create 3_load_and_store.s

---
+++ ARM/3_load_and_store.s
@@ -0,0 +1,19 @@
+.global _start
+
+.text
+_start:
+    
+	ldr r0, =var1 /* loads the address of var1 into r0 */
+	ldr r1, [r0]
+	
+	mov r2, #3
+	ldr r3, =var2 /* loads the address of var2 into r3 */
+	str r2, [r3] /* stores the value in r2 into the address pointed by r3 */
+	
+	 
+
+.data
+var1: .word 5
+var2: .word 7
+	
+	
