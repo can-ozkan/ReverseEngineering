8dfa0ea541c358471a73e8ac4066a986241d057c
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2025-04-25 13:46:12 +0200

    Create 2_add_sub_mul.s

---
+++ ARM/2_add_sub_mul.s
@@ -0,0 +1,18 @@
+.global _start
+_start:
+	
+    mov r0, #4
+	mov r2, #2
+	
+	add r1, r0, #3
+	add r1, r0, r2
+	
+	mov r0, #4
+	mov r2, #2
+	
+	sub r1, r0, r2
+	
+	mov r0, #4
+	mov r2, #2
+	
+	mul r1, r0, r2
