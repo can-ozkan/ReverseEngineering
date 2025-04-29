4e48e75233e04ce30a0d4b7021955cb3dd28c66f
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2025-04-29 14:41:51 +0200

    Create hello_world.s

---
+++ ARM/hello_world.s
@@ -0,0 +1,17 @@
+.global _start
+_start:
+	
+	mov r0, #1
+	ldr r1, =message
+	ldr r2, =len
+	mov r7, #4 
+	swi 0
+	
+	mov r7, #1
+	swi 0
+	
+	
+.data
+message:
+	.asciz "Hello Word\n" 
+len = .-message
