193d7ccadfe484b484d48c8710299028c4a2b4a5
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2025-04-29 14:34:43 +0200

    Update 3_load_and_store.s

--- ARM/3_load_and_store.s
+++ ARM/3_load_and_store.s
@@ -10,7 +10,8 @@ _start:
 	ldr r3, =var2 /* loads the address of var2 into r3 */
 	str r2, [r3] /* stores the value in r2 into the address pointed by r3 */
 	
-	 
+	mov r7, #1
+	swi 0
 
 .data
 var1: .word 5
