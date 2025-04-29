94c2266589ebb376865749cb6f49701363989950
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2025-04-29 08:48:15 +0200

    Create addressing_modes.s

---
+++ ARM/addressing_modes.s
@@ -0,0 +1,19 @@
+.global _start
+_start:
+	
+	// load data from stack to register
+	ldr r0, =list // it loads the address of the first element in list
+	
+	//how do we retrieve the value from the memory to registers?
+	ldr r1, [r0] // [] means go and fetch the value in that address
+	// like dereferencing in pointers
+	
+	ldr r2, [r0, #4]
+	
+.data
+list: 
+	.word 4, 5, -9, 1, 0, 2, -3
+	
+// list = [4, 5, -9, 1, 0, 2, -3]
+// list[r0]
+// list[r0 + 1]
